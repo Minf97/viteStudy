5ad86b2194d453e4afaf92f27c54525f9ed647d7
Minf97 <287728237@qq.com>
2023-10-30 16:06:34 +0800

    docs: merge

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,36 @@
 import { defineConfig, normalizePath } from 'vite';
 import react from '@vitejs/plugin-react';
-// 如果类型报错，需要安装 @types/node: pnpm i @types/node -D
+// ������ͱ�������Ҫ��װ @types/node: pnpm i @types/node -D
 import path from 'path';
 import autoprefixer from 'autoprefixer';
 import viteStylelint from 'vite-plugin-stylelint';
 
-// 用 normalizePath 解决 window 下的路径问题
+// �� normalizePath ��� window �µ�·������
 const variablePath = normalizePath(path.resolve('./src/variable.scss'));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // css 相关的配置
+  // css ��ص�����
   css: {
-    // 1. 预处理器
+    // 1. Ԥ������
     preprocessorOptions: {
-      // 这里是scss的配置，vite只是封装了一下，更多详细得去看scss文档
+      // ������scss�����ã�viteֻ�Ƿ�װ��һ�£�������ϸ��ȥ��scss�ĵ�
       scss: {
-        // additionalData 的内容会在每个 scss 文件的开头自动注入
-        additionalData: `@import "${variablePath}";`
+        // additionalData �����ݻ���ÿ�� scss �ļ��Ŀ�ͷ�Զ�ע��
+        additionalData: `@import '${variablePath}';`
       }
     },
     // 2. CSS Modules
     modules: {
-      // 通过 generateScopedName 控制生成的类名
-      // name表示当前文件名，local表示类名
+      // ͨ�� generateScopedName �������ɵ�����
+      // name��ʾ��ǰ�ļ�����local��ʾ����
       generateScopedName: '[name]__[local]__[hash:base64:5]'
     },
     // 3. PostCSS
     postcss: {
       plugins: [
         autoprefixer({
-          // 指定目标浏览器
+          // ָ��Ŀ�������
           overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11']
         })
       ]
@@ -39,18 +39,18 @@ export default defineConfig({
   plugins: [
     react({
       babel: {
-        // 加入babel 插件
-        // 一下插件包都需要提前安装
-        // 通过这个配置也可以添加其他 babel 插件
+        // ����babel ���
+        // һ�²��������Ҫ��ǰ��װ
+        // ͨ���������Ҳ������������ babel ���
         plugins: [
-          // 适配 styled-component
+          // ���� styled-component
           'babel-plugin-styled-components',
-          // 适配 emotion
+          // ���� emotion
           '@emotion/babel-plugin'
         ]
       },
-      // 对于 emotion，需要单独加上这个配置
-      // 通过这个包来编译 emotion 中的特殊 jsx 语法
+      // ���� emotion����Ҫ���������������
+      // ͨ������������� emotion �е����� jsx �﷨
       jsxImportSource: '@emotion/react'
     }),
     viteStylelint({
